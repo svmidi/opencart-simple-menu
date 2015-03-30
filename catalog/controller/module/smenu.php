7ab27566604adb78e8fa3aa2fe16aa4320b7abfa
SvM <svmidi@gmail.com>
2015-03-30 14:33:46 +0300

    Display module menu

--- catalog/controller/module/smenu.php
+++ catalog/controller/module/smenu.php
@@ -11,41 +11,66 @@ class ControllerModuleSmenu extends Controller {
 
 		$data['smenus'] = array();
 
-		$results = $this->model_catalog_smenu->getsmenu($setting['menu']);	
+		$root_items = $this->model_catalog_smenu->getSmenu($setting['menu']);
+		$routs=array(0 =>"/",1=>"information/contact", 2=>"account/return/add", 3=>"information/sitemap", 4=>"product/manufacturer", 5=>"account/voucher", 6=>"affiliate/account", 7=>"product/special", 8=>"account/account", 9=>"account/order", 10=>"account/wishlist", 11=>"account/newsletter", 12=>"account/newsletter");
+		$path=array(1=>'information/information', 2=>'product/category', 3 =>'catalog/product', 4=>'information/sigallery');
+		$path_url=array(1=>'information_id', 2=>'path', 3=>'path', 4=>'path_gallery');
 
-		/*foreach ($results as $result) {
-			$data['smenus'][] = array(
-					'title' => $result['smenu_title'],
-					'text' => $result['smenu_text'],
-					'link'  => $result['smenu_link']
-				);
-		}*/
+		foreach ($root_items as $items) {
+			$children_data=false;
+			$childs = $this->model_catalog_smenu->getSmenu($items['smenu_id'], $items['smenu_item_id']);
+			$active = 0;
 
-		foreach ($results as $menu_item) {
-			$children_data = array();
 
-			$children = $this->model_catalog_smenu->getsmenu($setting['menu'], $menu_item['smenu_item_id']);
-
-			foreach($children as $child) {
-				$children_data[] = array(
-					'title' => $child['smenu_title'],
-					'text' => $child['smenu_text'],
-					'link'  => $child['smenu_link']
-				);
+			if ($items['type']==5) {
+				$url=$items['type_name'];
+			}
+			elseif (($items['type']==6) AND ($items['type_id']!=0)) {
+				$url=$this->url->link($routs[(int)$items['type_id']],"", 'SSL');
+				if (isset($this->request->get['route']))
+				{
+					$active = ($this->request->get['route'] == $routs[(int)$items['type_id']])?'active':'';
+				}
+			}
+			elseif (($items['type']==6) AND ($items['type_id']==0)) {
+				$url="/";
+				$active = (!$this->request->get)?1:0;
+			}
+			else {
+				$url=$this->url->link($path[(int)$items['type']], "&".$path_url[(int)$items['type']]."=".$items['type_id'], 'SSL');
+				if ((isset($this->request->get['route']))AND($this->request->get['route']==$path[(int)$items['type']]) AND (isset($this->request->get[$path_url[(int)$items['type']]])) AND ($this->request->get[$path_url[(int)$items['type']]]==(int)$items['type_id']))
+					$active = 1;
 			}
 
-			$data['smenus'][] = array(
-				'title' => $menu_item['smenu_title'],
-				'text' => $menu_item['smenu_text'],
-				'link'  => $menu_item['smenu_link'],
-				'children'    => $children_data
+			if ($active) {
+				foreach ($childs as $child) {
+					if ($child['type']==5) {
+						$url=$items['type_name'];
+					}
+					if ($child['type']==6) {
+						$url=$this->url->link($routs[(int)$child['type']],"", 'SSL');
+					}
+					else {
+						$url=$this->url->link($path[(int)$child['type']], "&".$path_url[(int)$child['type']]."=".$child['type_id'], 'SSL');
+					}
+					$children_data[] = array(
+						'item_id'  => $child['smenu_item_id'],
+						'href'     => $url,
+						'name'     => $child['smenu_text'],
+						'title'    => $child['smenu_title']
+					);
+				}
+			}
+
+			$data['items'][] = array(
+				'item_id'        => $items['smenu_item_id'],
+				'name'           => $items['smenu_text'],
+				'title'          => $items['smenu_title'],
+				'href'           => $url,
+				'active'         => $active,
+				'children'       => $children_data
 			);
 		}
-		echo "<pre>";
-		print_r($data['smenus']);
-		echo "</pre>";
-	
-		//$data['module'] = $module++;
 				
 		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/module/smenu.tpl')) {
 			return $this->load->view($this->config->get('config_template') . '/template/module/smenu.tpl', $data);
--- catalog/view/theme/default/template/module/smenu.tpl
+++ catalog/view/theme/default/template/module/smenu.tpl
@@ -1,31 +1,19 @@
-<div class="box">
-  <div class="box-heading"><?php echo $heading_title; ?></div>
-  <div class="box-content">
-    <div class="box-gallery row">
-		<ul class="box-category">
-		  <?php foreach ($smenus as $smenu) { ?>
-		  <li>
-		    <?php if ($_SERVER['QUERY_STRING'] == $smenu['link']) { ?>
-		    <a href="<?php echo $smenu['link']; ?>" class="active" title="<?php echo $smenu['title']; ?>"><?php echo $smenu['text']; ?></a>
-		    <?php } else { ?>
-		    <a href="<?php echo $smenu['link']; ?>" title="<?php echo $smenu['title']; ?>"><?php echo $smenu['text']; ?></a>
-		    <?php } ?>
-		    <?php if ($smenu['children']) { ?>
-		    <ul>
-		      <?php foreach ($smenu['children'] as $child) { ?>
-		      <li>
-		        <?php if ($_SERVER['QUERY_STRING'] == $child['link']) { ?>
-		        <a href="<?php echo $child['link']; ?>" class="active" title="<?php echo $child['title']; ?>"> - <?php echo $child['text']; ?></a>
-		        <?php } else { ?>
-		        <a href="<?php echo $child['link']; ?>" title="<?php echo $child['title']; ?>"> - <?php echo $child['text']; ?></a>
-		        <?php } ?>
-		      </li>
-		      <?php } ?>
-		    </ul>
-		    <?php } ?>
-		  </li>
-		  <?php } ?>
-		</ul>
-	</div>
-  </div>
+<div class="box-heading"><?php echo $heading_title; ?></div>
+<div class="list-group">
+	<?php foreach ($items as $smenu) { ?>
+		<?php if ($smenu['active']) { ?>
+		<a href="<?php echo $smenu['href']; ?>" class="list-group-item active" title="<?php echo $smenu['title']; ?>"><?php echo $smenu['name']; ?></a>
+		<?php } else { ?>
+		<a href="<?php echo $smenu['href']; ?>" class="list-group-item" title="<?php echo $smenu['title']; ?>"><?php echo $smenu['name']; ?></a>
+		<?php } ?>
+		<?php if ($smenu['children']) { ?>
+			<?php foreach ($smenu['children'] as $child) { ?>
+				<?php if ($smenu['active']) { ?>
+				<a href="<?php echo $child['href']; ?>" class="list-group-item active" title="<?php echo $child['title']; ?>">&nbsp;&nbsp;&nbsp;- <?php echo $child['name']; ?></a>
+				<?php } else { ?>
+				<a href="<?php echo $child['href']; ?>" class="list-group-item" title="<?php echo $child['title']; ?>">&nbsp;&nbsp;&nbsp;- <?php echo $child['name']; ?></a>
+				<?php } ?>
+			<?php } ?>
+		<?php } ?>
+	<?php } ?>
 </div>
